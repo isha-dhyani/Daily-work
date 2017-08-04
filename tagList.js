865270fa37571e3160dc6c547a203be5111dbac7
isha-dhyani <isha.dhyani25@gmail.com>
2017-08-04 16:07:49 +0530

    Create tagList.js

---
+++ tagList.js
@@ -0,0 +1,15 @@
+
+var ar= document.getElementById("jsList").value;
+console.log(ar);
+var obj=JSON.parse(ar);
+console.log(obj);
+function getTagList(id){
+    var value = obj[id];
+
+    var list="";
+    for(var i=0; i<value.length;i++){
+
+        list +="<div class=\"td-pb-span4\"><a href='/isha/index.php/tag/"+value[i][1]+"'>"+value[i][0]+"</a></div>";
+    }
+    document.getElementById("list").innerHTML=list;
+}
