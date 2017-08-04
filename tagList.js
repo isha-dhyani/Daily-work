1a353e4f9bf85977a5859f3b724b426c619eca38
isha-dhyani <isha.dhyani25@gmail.com>
2017-08-04 16:12:34 +0530

    Update tagList.js

--- tagList.js
+++ tagList.js
@@ -9,7 +9,7 @@ function getTagList(id){
     var list="";
     for(var i=0; i<value.length;i++){
 
-        list +="<div class=\"td-pb-span4\"><a href='/isha/index.php/tag/"+value[i][1]+"'>"+value[i][0]+"</a></div>";
+        list +="<div class=\"td-pb-span4\"><a href='/isha/index.php/tag/"+value[i][1]+"' target='_blank'>"+value[i][0]+"</a></div>";
     }
     document.getElementById("list").innerHTML=list;
 }
